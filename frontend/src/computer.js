923a74bb12dcd551699393253c3f0356bbd0e475
guerino antonini <guerantonini94@gmail.com>
2021-05-12 19:34:49 -0400

    WIP- rendered computer list as well as created parts-list form

--- frontend/src/computer.js
+++ frontend/src/computer.js
@@ -4,7 +4,6 @@ let newComputerForm = document.getElementById("new-computer-form")
 
 newComputerForm.addEventListener("submit", function(event){
     event.preventDefault()
-    // console.log("event", event.target[0].value)
     createNewComputer(event.target)
 })
 
@@ -20,5 +19,28 @@ function createNewComputer(computer_info){
             }
         })
     })
+    .then(resp => resp.json())
+    .then(resp => fetchComputers(resp))
 
 }
+
+function fetchComputers(){
+    fetch("http://localhost:3000/computers")
+    .then(resp => resp.json())
+    .then(json => renderComputers(json))
+
+}
+
+fetchComputers();
+
+function renderComputers(computers){
+    const form = document.getElementById('computer-list')
+    computers.forEach(element => {
+        const h2 = document.createElement('h2')
+        h2.innerHTML = element.name
+        form.appendChild(h2);
+        
+    });
+}
+
+
